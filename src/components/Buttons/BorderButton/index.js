551cbf0331032ff9c7e6692b20b5c40cb6dcc7b1
tujiaw <tujiaw@163.com>
2017-07-30 23:21:09 +0800

    button样式

--- src/components/Buttons/BorderButton/index.js
+++ src/components/Buttons/BorderButton/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Style from './style'
 
 class BorderButton extends React.Component {
+  state = {
+    isEnter: false
+  }
+
   render() {
     return (
-      <button onClick={this.props.onClick}>{this.props.text}</button>
+      <button style={this.state.isEnter ? Style.enterButton : Style.button} 
+        onClick={this.props.onClick}
+        onMouseEnter={() => this.setState({ isEnter: true })}
+        onMouseLeave={() => this.setState({ isEnter: false })}>
+        {this.props.text}
+      </button>
     );
   }
 }
---
+++ src/components/Buttons/BorderButton/style.js
@@ -0,0 +1,17 @@
+const Style = {}
+Style.button = {
+  fontSize: 16,
+  background: '#f1f1f1',
+  color: '#666',
+  border: '1px solid #666',
+  padding: '5px 30px',
+  outline: '0px'
+}
+
+Style.enterButton = {
+  ...Style.button,
+  color: 'rgb(233, 32, 61)',
+  border: '1px solid rgb(233, 32, 61)',
+}
+
+export default Style
